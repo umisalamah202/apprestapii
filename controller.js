6b9a094b6632c6638c1c0db2a8765f6eaca680aa
umisalamah <umis0179@outlook.com>
2020-04-04 09:20:40 +0700

    behasil ubah data

--- controller.js
+++ controller.js
@@ -47,3 +47,20 @@ exports.tambahMahasiswa = function (req,res) {
         }
     });
 };
+
+//mengubah data berdasarkan id
+exports.ubahMahasiswa = function(req,res) {
+    var id = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama =req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('UPDATE mahasiswa SET nim=?, nama=?, jurusan=?',[nim,nama,jurusan],
+        function(error, rows, fileds){
+            if(error){
+                console.log(error);
+            }else {
+                response.ok("Berhasil Ubah Data", res)
+            }
+        });
+}
